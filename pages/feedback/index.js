28b33c0c9a6e61c9b8f299dd788b9c1a0eef47c0
nader <esmailjafarpour29@gmail.com>
2022-11-10 12:00:58 +0330

    create apiRoute project

---
+++ pages/feedback/index.js
@@ -0,0 +1,38 @@
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import { useState, Fragment } from 'react';
+
+function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    function loadFeedbackHandler(id){
+        fetch(`/api/${id}`)
+            .then((response) => response.json())
+            .then((data) => {setFeedbackData(data.feedback)}) 
+    }
+
+    return (
+    <Fragment>
+        { feedbackData && <p>{feedbackData.email}</p> }
+        <ul className='feedback'>
+            {props.feedbackItems.map((item)=> (
+                <li key={item.id}>
+                    {item.text}
+                    <button className='btn' onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button>
+                </li>
+            ))}
+        </ul>
+    </Fragment>
+    );
+}
+
+export async function getStaticProps() {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    return {
+        props: {
+            feedbackItems: data,
+        }
+    }
+}
+
+export default FeedbackPage;
